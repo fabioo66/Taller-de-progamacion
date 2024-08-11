f5e9e1fd8e064f96635cb078ec290b73d9212317
fabioo66 <torrejonfabio@gmail.com>
2024-08-11 19:56:01 -0300

    Practica 3 arboles

--- MODULO IMPERATIVO/Practicas resueltas/Practica 3/ejercicio1.pas
+++ MODULO IMPERATIVO/Practicas resueltas/Practica 3/ejercicio1.pas
@@ -75,6 +75,7 @@ begin
     end;
 end;
 
+{i}
 function numSocioMasGrande(a: arbol): integer;
 begin
     if(a^.HD = nil) then
@@ -83,6 +84,7 @@ begin
         numSocioMasGrande := numSocioMasGrande(a^.HD);
 end;
 
+{ii}
 procedure datosSocioMasChico(a: arbol; var s: socio);
 begin
     if (a^.HI = nil) then
@@ -91,16 +93,131 @@ begin
         datosSocioMasChico(a^.HI, s);
 end;
 
+{iii}
 procedure numSocioMayorEdad(a: arbol; max: integer; var num: integer);
 begin
-    if (a^.dato.edad > max) then begin
-        max := a^.dato.edad;
-        num := a^.dato.numSocio;
-    end;
-    if (a^.HI <> nil) then
+    if (a <> nil) then begin
+        if (a^.dato.edad > max) then begin
+            max := a^.dato.edad;
+            num := a^.dato.numSocio;
+        end;
         numSocioMayorEdad(a^.HI, max, num);
-    if (a^.HD <> nil) then
         numSocioMayorEdad(a^.HD, max, num);
+    end;
 end;
 
+{iv}
 procedure aumentarEdad(a: arbol);
+begin
+    if (a <> nil) then begin
+        a^.dato.edad := a^.dato.edad + 1;
+        aumentarEdad(a^.HI);
+        aumentarEdad(a^.HD);
+    end;
+end;
+
+{v}
+function existeNumSocio(a: arbol; num: integer): boolean;
+begin
+    if (a = nil) then
+        existeNumSocio := false
+    else begin
+        if (a^.dato.numSocio = num) then
+            existeNumSocio := true
+        else begin
+            if (num < a^.dato.numSocio) then
+                existeNumSocio := existeNumSocio(a^.HI, num)
+            else
+                existeNumSocio := existeNumSocio(a^.HD, num);
+        end;
+    end;
+end;
+
+{vi}
+function existeNombre(a: arbol; nombre: string): boolean;
+begin
+    if (a <> nil) then begin
+        if (a^.dato.nombre = nombre) then
+            existeNombre := true
+        else
+            existeNombre := existeNombre(a^.HI, nombre) or existeNombre(a^.HD, nombre);
+    end
+    else
+        existeNombre := false;
+end;
+
+{vii}
+function cantidadSocios(a: arbol): integer;
+begin
+    if (a = nil) then
+        cantidadSocios := 0
+    else
+        cantidadSocios := 1 + cantidadSocios(a^.HI) + cantidadSocios(a^.HD);
+end;
+
+{viii}
+function sumaEdades(a: arbol): integer;
+begin
+    if (a = nil) then
+        sumaEdades := 0
+    else
+        sumaEdades := a^.dato.edad + sumaEdades(a^.HI) + sumaEdades(a^.HD);
+end;
+
+{ix}
+procedure numerosSocioEnOrden(a: arbol);
+begin
+    if (a <> nil) then begin
+        numerosSocioEnOrden(a^.HI);
+        writeln(a^.dato.numSocio);
+        numerosSocioEnOrden(a^.HD);
+    end;
+end;
+
+{x}
+procedure numerosSocioParesEnOrdenDecreciente(a: arbol);
+begin
+    if (a <> nil) then begin
+        numerosSocioParesEnOrdenDecreciente(a^.HD);
+        numerosSocioParesEnOrdenDecreciente(a^.HI);
+        if (a^.dato.numSocio mod 2 = 0) then
+            writeln(a^.dato.numSocio);
+    end;
+end;
+
+var
+    a: arbol;
+    s: socio;
+    num: integer;
+    nombre: string;
+begin
+    a := nil;
+    cargarArbol(a);
+    writeln('Numero de socio mas grande: ', numSocioMasGrande(a));
+    datosSocioMasChico(a, s);
+    writeln('Datos del socio con el numero de socio mas chico: ');
+    writeln('Numero de socio: ', s.numSocio);
+    writeln('Nombre: ', s.nombre);
+    writeln('Edad: ', s.edad);
+    numSocioMayorEdad(a, 0, num);
+    writeln('Numero de socio con mayor edad: ', num);
+    aumentarEdad(a);
+    writeln('Ingrese un numero de socio: ');
+    readln(num);
+    if (existeNumSocio(a, num)) then
+        writeln('Existe un socio con ese numero')
+    else
+        writeln('No existe un socio con ese numero');
+    writeln('Ingrese un nombre: ');
+    readln(nombre);
+    if (existeNombre(a, nombre)) then
+        writeln('Existe un socio con ese nombre')
+    else
+        writeln('No existe un socio con ese nombre');
+    writeln('Cantidad de socios: ', cantidadSocios(a));
+    writeln('Promedio de edad de los socios: ', sumaEdades(a) / cantidadSocios(a));
+    writeln('Numeros de socio en orden creciente: ');
+    numerosSocioEnOrden(a);
+    writeln('Numeros de socio pares en orden decreciente: ');
+    numerosSocioParesEnOrdenDecreciente(a);
+end.
--- MODULO IMPERATIVO/Practicas resueltas/Practica 3/ejercicio3.pas
+++ MODULO IMPERATIVO/Practicas resueltas/Practica 3/ejercicio3.pas
@@ -1,4 +1,4 @@
-{3. Implementar un programa que contenga:
+{Implementar un programa que contenga:
 a. Un módulo que lea información de alumnos de Taller de Programación y los almacene en
 una estructura de datos. De cada alumno se lee legajo, DNI, año de ingreso y los códigos y
 notas de los finales rendidos. La estructura generada debe ser eficiente para la búsqueda por
@@ -15,216 +15,214 @@ alumno con mayor promedio.
 g. Un módulo que reciba la estructura generada en a. y un valor entero. Este módulo debe
 retornar los legajos y promedios de los alumnos cuyo promedio supera el valor ingresado.}
 
-
-program Hello;
+program ejercicio3;
 type
-  final = record
-    codigo:integer;
-    nota:integer;
-  end;
-
-  lista = ^nodo;
-  nodo = record
-    dato:final;
-    sig:lista;
-  end;
-  alumno = record
-    legajo:integer;
-    dni:integer;
-    anio:integer;
-    finales:lista;
-  end;
-  
-  arbol = ^nodo1;
-  nodo1 = record
-    dato:alumno;
-    HI:arbol;
-    HD:arbol;
-  end;
-  
-  
-procedure leerfinal(var f:final);
-begin
-  writeln('ingrese el codigo del final');
-  readln(f.codigo);
-  writeln('ingrese la nota del final');
-  readln(f.nota);
-end;
-
-procedure agregarAdelante(var L:lista; f:final);
-var
-  nue:lista;
-begin
-  new(nue);
-  nue^.dato:= f;
-  nue^.sig:= L;
-  L:= nue;
-end;
-
-procedure cargarlista(var L:lista);
-var
-  f:final;
-begin
-  leerfinal(f);
-  while (f.codigo <> -1) do begin
-    agregarAdelante(L,f);
-    leerfinal(f);
-  end;
-end;
-
-procedure leeralumno(a:alumno);
-begin
-  writeln('ingrese el numero de legajo');
-  readln(a.legajo);
-  if (a.legajo <> 0) then begin
-    writeln('ingrese el numero de dni');
-    readln(a.dni);
-    writeln('ingrese el anio');
-    readln(a.anio);
-    cargarlista(a.finales);
-  end;
-end;
-
-procedure agregar(var a:arbol; alum:alumno);
-begin
-  if (a = nil) then begin
-    new(a);
-    a^.dato:= alum;
-    a^.HI:= nil;
-    a^.HD:= nil;
-  end
-  else if (alum.legajo <= a^.dato.legajo) then
-    agregar(a^.HI,alum)
-  else
-    agregar(a^.HD,alum);
-end;
-  
-procedure cargararbol(var a:arbol);
-var
-  alum:alumno;
-begin
-  leeralumno(alum);
-  if (alum.legajo <> 0) then begin
-    agregar(a,alum);
-    cargararbol(a);
-  end;
-end;
-
-{b. Un módulo que reciba la estructura generada en a. y retorne los DNI y año de ingreso de
-aquellos alumnos cuyo legajo sea inferior a un valor ingresado como parámetro.}
-
-procedure legajoinf(a:arbol; valor:integer);
-begin
-  if (a <> nil) then begin
-    legajoinf(a^.HI,valor);
-    legajoinf(a^.HD,valor);
-    if (a^.dato.legajo < valor) then 
-      writeln('el nro de legajo de este alumno es menor a ' , valor ,' : ', a^.dato.dni, a^.dato.anio);
-  end;
-end;
-
-{c. Un módulo que reciba la estructura generada en a. y retorne el legajo más grande.}
-
-procedure mayorlegajo (a:arbol; var max:integer);
-begin
-  if (a <> nil) then begin
-    max:= a^.dato.legajo;
-    mayorlegajo(a^.HD,max);
-  end;
-end;
-
-{d. Un módulo que reciba la estructura generada en a. y retorne el DNI más grande.}
-
-procedure mayordni(a:arbol; var maxdni:integer);
-begin
-  if (a <> nil) then begin
-    if (a^.dato.dni > maxdni) then
-      maxdni:= a^.dato.dni;
-    mayordni(a^.HI,maxdni);
-    mayordni(a^.HD,maxdni);
-  end;
-end;
-
-{e. Un módulo que reciba la estructura generada en a. y retorne la cantidad de alumnos con
-legajo impar.}
-
-procedure legajoimpar(a:arbol; var cant:integer);
-begin
-  if (a <> nil) then begin
-    if (a^.dato.legajo mod 2 <> 0) then
-      cant:= cant + 1;
-    legajoimpar(a^.HI,cant);
-    legajoimpar(a^.HD,cant);
-  end;
-end;
-
-{f. Un módulo que reciba la estructura generada en a. y retorne el legajo y el promedio del
-alumno con mayor promedio.}
-
-function promedio(L:lista):real;
-var
-  cantnotas,cantexamenes:integer;
-begin
-  cantnotas:= 0;
-  cantexamenes:= 0;
-  while (L <> nil) do begin
-    cantexamenes:= cantexamenes + 1;
-    cantnotas:= cantnotas + L^.dato.nota;
-    L:= L^.sig;
-  end;
-    promedio:= (cantnotas/cantexamenes);
-end;
-
-procedure mayorpromedio(a:arbol; var legajomax:integer; var prom:real);
-var
-  p:real;
-begin
-  if (a <> nil) then begin
-    p:= promedio(a^.dato.finales);
-    if (p > prom) then begin
-      prom:= p;
-      legajomax:= a^.dato.legajo;
+    final = record
+        codMateria: integer;
+        nota: real;
     end;
-    mayorpromedio(a^.HI,legajomax,prom);
-    mayorpromedio(a^.HD,legajomax,prom);
-  end;
+
+    alumno = record
+        legajo: integer;
+        DNI: integer;
+        anioIngreso: integer;
+        finales: lista;
+    end;
+
+    lista = ^nodo;
+    nodo = record
+        dato: final;
+        sig: lista;
+    end;
+
+    arbol = ^nodoArbol;
+    nodoArbol = record
+        dato: alumno;
+        HI: arbol;
+        HD: arbol;
+    end;   
+
+procedure leerFinal(var f: final);
+begin
+    writeln('Ingrese el codigo de materia: ');
+    readln(f.codMateria);
+    if (f.codMateria <> -1) then begin
+        writeln('Ingrese la nota del final: ');
+        readln(f.nota);
+    end;
 end;
 
-{g. Un módulo que reciba la estructura generada en a. y un valor entero. Este módulo debe
-retornar los legajos y promedios de los alumnos cuyo promedio supera el valor ingresado.}
-
-procedure promediosup(a:arbol; valor:real);
+procedure insertarFinal(var l: lista; f: final);
 var
-  p:real;
+    aux: lista;
 begin
-  if (a <> nil) then begin
-    p:= promedio(a^.dato.finales);
-    if (p > valor) then
-      writeln('el promedio de este alumno supera al numero ingresado', a^.dato.legajo, ' promedio: ', p);
-    promediosup(a^.HI,valor);
-    promediosup(a^.HD,valor);
-  end;
+    new(aux);
+    aux^.dato := f;
+    aux^.sig := l;
+    l := aux;
+end;
+
+procedure leerAlumno(var a: alumno);
+var
+    f: final;
+begin
+    a.finales := nil;
+    writeln('Ingrese el legajo del alumno: ');
+    readln(a.legajo);
+    if (a.legajo <> 0) then begin
+        writeln('Ingrese el DNI del alumno: ');
+        readln(a.DNI);
+        writeln('Ingrese el año de ingreso del alumno: ');
+        readln(a.anioIngreso);
+        a.cantFinales := 0;
+        a.finales := nil;
+        leerFinal(f);
+        while (f.codMateria <> -1) do begin
+            a.cantFinales := a.cantFinales + 1;
+            insertarFinal(a.finales, f);
+            leerFinal(f);
+        end;
+    end;
+end;
+
+procedure insertar(var a: arbol; al: alumno);
+begin
+    if (a = nil) then begin
+        new(a);
+        a^.dato := al;
+        a^.HI := nil;
+        a^.HD := nil;
+    end
+    else
+        if (al.legajo <= a^.dato.legajo) then
+            insertar(a^.HI, al)
+        else
+            insertar(a^.HD, al);
+end;
+
+{a}
+procedure cargarArbol(var a: arbol);
+var
+    al: alumno;
+begin
+    leerAlumno(al);
+    while (al.legajo <> 0) do begin
+        insertar(a, al);
+        leerAlumno(al);
+    end;
+end;
+
+{b}
+procedure DNIyAnioIngresoMenorA(a: arbol; valor: integer);
+begin
+    if (a <> nil) then begin
+        if (a^.dato.legajo < valor) then
+            writeln('DNI: ', a^.dato.DNI, ' Anio de ingreso: ', a^.dato.anioIngreso);
+        DNIyAnioIngresoMenorA(a^.HI, valor);
+        DNIyAnioIngresoMenorA(a^.HD, valor);
+    end;
+end;
+
+{c}
+function legajoMasGrande(a: arbol): integer;
+begin
+    if (a^.HD = nil) then
+        legajoMasGrande := a^.dato.legajo
+    else
+        legajoMasGrande := legajoMasGrande(a^.HD);
+end;
+
+{d}
+procedure DNIgrande(a: arbol; var max: integer);
+begin
+    if (a <> nil) then begin
+        if (a^.dato.DNI > max) then
+            max := a^.dato.DNI;
+        DNIgrande(a^.HI, max);
+        DNIgrande(a^.HD, max);
+    end;
+end;
+
+{e}
+procedure cantAlumnosLegajoImpar(a: arbol; var cant: integer);
+begin
+    if (a <> nil) then begin
+        if (a^.dato.legajo mod 2 <> 0) then
+            cant := cant + 1;
+        cantAlumnosLegajoImpar(a^.HI, cant);
+        cantAlumnosLegajoImpar(a^.HD, cant);
+    end;
+end;
+
+{f}
+procedure devolverPromedio(l: lista; var promedio: real);
+var
+    cant: integer;
+    suma: real;
+begin
+    suma := 0;
+    cant := 0;
+    while (l <> nil) do begin
+        suma := suma + l^.dato.nota;
+        cant := cant + 1;
+        l := l^.sig;
+    end;
+    promedio := suma / cant;
+end;
+
+{f}
+procedure promedioMayor(a: arbol; var max: real; var legajo: integer);
+var 
+    promedio: real;
+begin
+    if (a <> nil) then begin
+        devolverPromedio(a^.dato.finales, promedio);
+        if (promedio > max) then begin
+            max := promedio;
+            legajo := a^.dato.legajo;
+        end;
+        promedioMayor(a^.HI, max, legajo);
+        promedioMayor(a^.HD, max, legajo);
+    end;
+end;
+
+{g}
+procedure promedioMayorA(a: arbol; valor: real);
+begin
+    if (a <> nil) then begin
+        devolverPromedio(a^.dato.finales, promedio);
+        if (promedio > valor) then
+            writeln('Legajo: ', a^.dato.legajo, ' Promedio: ', promedio);
+        promedioMayorA(a^.HI, valor);
+        promedioMayorA(a^.HD, valor);
+    end;
 end;
 
 var
-  a:arbol;
-  num,legajoprom,valor:integer;
-  maxlegajo,maxdni:integer;
-  prom:real;
+    a: arbol;
+    valor: integer;
+    max: integer;
+    cant: integer;
+    promedio: real;
 begin
-  cargararbol(a);
-  
-  read(num);
-  legajoinf(a,num);
-  
-  mayorlegajo(a,maxlegajo);
-  
-  maxdni:= -1;
-  mayordni(a,maxdni);
-  
-  prom:= -1;
-  mayorpromedio(a,legajoprom,prom);
-  
-  read(valor);
-  promediosup(a,valor);
+    a := nil;
+    cargarArbol(a);
+    writeln('Ingrese un valor para buscar los alumnos con legajo menor a ese valor: ');
+    readln(valor);
+    DNIyAnioIngresoMenorA(a, valor);
+    max := legajoMasGrande(a);
+    writeln('El legajo mas grande es: ', max);
+    max := 0;
+    DNIgrande(a, max);
+    writeln('El DNI mas grande es: ', max);
+    cant := 0;
+    cantAlumnosLegajoImpar(a, cant);
+    writeln('La cantidad de alumnos con legajo impar es: ', cant);
+    max := 0;
+    promedio := 0;
+    promedioMayor(a, promedio, max);
+    writeln('El legajo del alumno con mayor promedio es: ', max, ' con un promedio de: ', promedio:2:2);
+    writeln('Ingrese un valor para buscar los alumnos con promedio mayor a ese valor: ');
+    readln(valor);
+    promedioMayorA(a, valor);
 end.
